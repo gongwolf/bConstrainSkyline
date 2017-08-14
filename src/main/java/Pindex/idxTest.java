223b0327c34ff96981ce87f830eb5ea39a27aea1
gongwolf <gongwolf@gmail.com>
2017-08-13 23:48:18 -0600

    builded index

--- src/main/java/Pindex/idxTest.java
+++ src/main/java/Pindex/idxTest.java
@@ -1,10 +1,12 @@
 package Pindex;
 
 import neo4jTools.BNode;
+import neo4jTools.Line;
 import neo4jTools.connector;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphalgo.GraphAlgoFactory;
+import org.neo4j.graphalgo.PathFinder;
+import org.neo4j.graphalgo.WeightedPath;
+import org.neo4j.graphdb.*;
 
 import java.util.ArrayList;
 
@@ -18,6 +20,9 @@ public class idxTest {
         String sid = "1";
         String eid = "99";
         t.runIdxSkyline(sid,eid,graphdb);
+        long ct = System.nanoTime();
+        t.getShortestCost(sid,eid,graphdb);
+        System.out.println((System.nanoTime()-ct)/1000000);
         n.shutdownDB();
 
     }
@@ -34,5 +39,53 @@ public class idxTest {
         indexSkyline idxNode = new indexSkyline(graphdb);
         ArrayList<path> r = idxNode.getSkylinePath(Source, Destination);
         return r;
+    }
+
+    public ArrayList<path> runUseNodeFinal(String sid, String did, GraphDatabaseService graphdb) {
+//        connector n = new connector("/home/gqxwolf/neo4j/csldb/databases/graph.db");
+        Node Source;
+        Node Destination;
+        try (Transaction tx = graphdb.beginTx()) {
+            Source = graphdb.findNode(BNode.BusNode, "name", sid);
+            Destination = graphdb.findNode(BNode.BusNode, "name", did);
+            tx.success();
+        }
+        myshortestPathUseNodeFinal mspNode = new myshortestPathUseNodeFinal(graphdb);
+        ArrayList<path> r = mspNode.getSkylinePath(Source, Destination);
+        return r;
+    }
+
+    public double[] getShortestCost(String sid, String did, GraphDatabaseService graphdb) {
+//        connector n = new connector("/home/gqxwolf/neo4j/csldb/databases/graph.db");
+
+        Node Source;
+        Node Destination;
+        double[] iniLowerBound;
+        try (Transaction tx = graphdb.beginTx()) {
+            Source = graphdb.findNode(BNode.BusNode, "name", sid);
+            Destination = graphdb.findNode(BNode.BusNode, "name", did);
+            path dummyP = new path(Source);
+            int i = 0;
+            iniLowerBound = new double[dummyP.NumberOfProperties];
+
+            for (int j = 0; j < iniLowerBound.length; j++) {
+                iniLowerBound[j] = -1;
+            }
+
+            for (String property_name : dummyP.propertiesName) {
+                PathFinder<WeightedPath> finder = GraphAlgoFactory
+                        .dijkstra(PathExpanders.forTypeAndDirection(Line.Linked, Direction.OUTGOING), property_name);
+                WeightedPath paths = finder.findSinglePath(dummyP.endNode, Destination);
+                if (paths != null) {
+                    iniLowerBound[i++] = paths.weight();
+                } else {
+                    break;
+                }
+            }
+            tx.success();
+            // System.out.println(printCosts(iniLowerBound));
+        }
+
+        return iniLowerBound;
     }
 }
